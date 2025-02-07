9ff453b3ccf021e81f9a74e3f17e6d9403ab83f3
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-02-07 16:55:40 +0100

    added request and router classes

---
+++ src/http/Router.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http;
+
+use App\Exceptions\NotFoundHttpException;
+
+class Router
+{
+    public function __construct(public array $routes) {}
+
+    public function direct(string $uri): string
+    {
+        if (array_key_exists($uri, $this->routes)) {
+            return $this->routes[$uri];
+        }
+        throw new NotFoundHttpException('No route defined for this URI');
+    }
+}
