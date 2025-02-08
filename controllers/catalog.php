0ce9eb8b1756b1a646d0605cf8c8cd3d0981c80d
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-02-08 15:15:14 +0100

    added further logic

--- controllers/catalog.php
+++ controllers/catalog.php
@@ -2,9 +2,31 @@
 
 declare(strict_types=1);
 
+require __DIR__ . '/../init.php';
+
+use App\Shopping\Product;
+
 function loadCatalog(): array
 {
-    $json = file_get_contents(__DIR__ . 'data/products.json');
+    $json = file_get_contents(__DIR__ . '/../data/products.json');
     $data = json_decode($json, true);
-    return $data['catalog'] ?? [];
+
+    if (!isset($data['catalog'])) {
+        return [];
+    }
+
+    $products = [];
+    foreach ($data['catalog'] as $item) {
+        $products[] = new Product(
+            $item['id'],
+            $item['name'],
+            $item['category'],
+            $item['price']
+        );
+    }
+
+    return $products;
 }
+
+$products = loadCatalog();
+require __DIR__ . '/../views/catalog.view.php';
