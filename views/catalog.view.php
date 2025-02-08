4690d247cd26994b3be9fdad5d8787e9b90599b4
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-02-08 15:25:49 +0100

    change language

--- views/catalog.view.php
+++ views/catalog.view.php
@@ -4,17 +4,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Produktkatalog</title>
+    <title>Products</title>
 </head>
 
 <body>
-    <h1>Produktkatalog</h1>
+    <h1>Products</h1>
     <ul>
         <?php foreach ($products as $product): ?>
             <li>
                 <strong><?= htmlspecialchars($product->getName()) ?></strong><br>
-                Kategori: <?= htmlspecialchars($product->getCategory()) ?><br>
-                Pris: <?= number_format($product->getPrice(), 2, ',', ' ') ?> kr
+                Category: <?= htmlspecialchars($product->getCategory()) ?><br>
+                Price: <?= number_format($product->getPrice(), 2, ',', ' ') ?> SEK
             </li>
         <?php endforeach; ?>
     </ul>
